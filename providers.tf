c168e5ba3bd278a36ca0883f02f0b9db9d067e0d
Joffrey Dupire <joffrey.dupire@gmail.com>
2025-03-10 14:42:20 +0100

    plip

--- providers.tf
+++ providers.tf
@@ -18,22 +18,3 @@ provider "azurerm" {
   subscription_id = "7010a62d-1c01-44b2-b84e-9e38000f3f54"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = ".kube/config"
-  }
-
-  # localhost registry with password protection
-  /*registry {
-    url = "oci://localhost:5000"
-    username = "username"
-    password = "password"
-  }
-
-  # private registry
-  registry {
-    url = "oci://private.registry"
-    username = "username"
-    password = "password"
-  }*/
-}
--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,3 @@
-/*module "network" {
-  source = "../azure_resource_group"
-
-  location      = var.location
-  subnet_config = var.subnet_config
-}*/
 
 resource "azurerm_resource_group" "rg" {
   name = "plop"
@@ -27,49 +21,4 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   tags = var.tags
-}
-
-/*
- J'ai maintenant le cluster pret a acceuillir des pods/servicse etc cependant je n'ai aucun Ingress Controller.
- En utilisant Helm Chart je déploie mon controller nginx (j'utilise un chart de la communauté) et je fais pareil
- avec cert-manager qui est l'outils pour gérer les certificats HTTPS, et enfin redis.
-
- Je passe volontairement avec les Charts de la communauté pour simplifier, je n'ai pas besoin de re-coder
- quelque chose que le constructeur fera mieux que moi! Cependant ça ne m'empeche pas d'aller voir comment 
- ils ont fait (On ne sait jamais dans certains cas je pourrais avoir a le faire moi même).
-
- N'oubliez pas l'acronyme KISS (Keep It Simple Stupid https://en.wikipedia.org/wiki/KISS_principle ), 
- ou le rasoir d'Occam (shorturl.at/eBEFV)
-*/
-
-# UPDATE YOUR KUBE CONFIG OTHERWISE HELM WILL NOT BE ABLE TO DEPLOY THE CHART 
-
-
-resource "local_file" "kube_config" {
-  content  = azurerm_kubernetes_cluster.aks.kube_config_raw
-  filename = ".kube/config"
-}
-
-resource "helm_release" "chart" {
-  for_each         = var.charts
-  name             = each.key
-  namespace        = each.key
-  create_namespace = each.value.create_namespace
-  repository       = each.value.repository
-  chart            = each.key
-  version          = each.value.version
-
-  dynamic "set" {
-    for_each = each.value.sets
-    content {
-      name = set.key
-      value = set.value
-    }
-  }
-
-  depends_on = [
-    azurerm_kubernetes_cluster.aks,
-    azurerm_resource_group.rg,
-    local_file.kube_config
-  ]
 }
